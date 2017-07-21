9e8f83ea614be7f80c989fb15552579e3b1c93eb
shuidi_iMac <shuidi_imac@shuidiiacdeimac.lan>
2017-07-21 17:33:30 +0800

    Initial Commit

---
+++ EmulateWeChatDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  EmulateWeChatDemo
+//
+//  Created by shuidi_iMac on 2017/7/21.
+//  Copyright © 2017年 shuidi_iMac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
