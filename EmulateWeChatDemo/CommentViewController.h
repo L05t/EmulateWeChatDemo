df9c3283907d409a33e0c776681e43e05dbc3149
shuidi_iMac <shuidi_imac@shuidiiacdeiMac.lan>
2017-07-21 18:25:57 +0800

    first commit

---
+++ EmulateWeChatDemo/CommentViewController.h
@@ -0,0 +1,13 @@
+//
+//  CommentViewController.h
+//  EmulateWeChatDemo
+//
+//  Created by shuidi_iMac on 2017/7/21.
+//  Copyright © 2017年 shuidi_iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommentViewController : UIViewController
+
+@end
